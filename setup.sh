02bb7dc8c01465b5905c0d7672d51cc12d86fafc
Mark desktop <mark.pc.lee@gmail.com>
2023-03-10 20:21:19 +0800

    ok

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
-git clone https://github.com/ASUS-AICS/LibMultiLabel.git
-cd LibMultiLabel 
-source /home/faculty/cjlin/\
-miniconda3/bin/activate LibMultiLabel
-mkdir -p /tmp2/$USER/runs
-mkdir -p /tmp2/$USER/.vector_cache
-ln -s /tmp2/$USER/runs runs
-ln -s /tmp2/$USER/.vector_cache .vector_cache
-mkdir -p data/rcv1
-cd data/rcv1
-wget -O train.txt.bz2 \
-https://www.csie.ntu.edu.tw/~cjlin/\
-libsvmtools/datasets/multilabel/\
-rcv1_topics_train.txt.bz2
-bzip2 -d *.bz2
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
