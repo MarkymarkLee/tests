2689c4eb1e55578deb04f28a744f37130796175a
Mark desktop <mark.pc.lee@gmail.com>
2023-03-10 20:05:14 +0800

    ok

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-git clone git@github.com:NTU-SP/hw2_pvp-championship-MarkymarkLee.git
+sudo apt update
+sudo apt install git build-essential libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
+
+git clone https://gitlab.com/qemu-project/qemu.git
+cd qemu/
+git checkout tags/v6.1.0
+
+./configure --target-list=x86_64-softmmu --disable-werror
+make -j4
+
+sudo make install
+cd ..
+
+wget -O guest.img https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-nocloudamd64.qcow2
+qemu-img resize guest.img 10g
